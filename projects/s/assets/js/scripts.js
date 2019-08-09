c0ecf171bec8bb5b35ddec9536b9058dee046aab
Denis Shibidzhiev <32371070+Renko-hens@users.noreply.github.com>
2019-08-09 17:43:56 +0300

    Add files via upload

---
+++ projects/s/assets/js/scripts.js
@@ -0,0 +1,470 @@
+// Utility function
+function Util () {};
+
+/* 
+	class manipulation functions
+*/
+Util.hasClass = function(el, className) {
+	if (el.classList) return el.classList.contains(className);
+	else return !!el.className.match(new RegExp('(\\s|^)' + className + '(\\s|$)'));
+};
+
+Util.addClass = function(el, className) {
+	var classList = className.split(' ');
+ 	if (el.classList) el.classList.add(classList[0]);
+ 	else if (!Util.hasClass(el, classList[0])) el.className += " " + classList[0];
+ 	if (classList.length > 1) Util.addClass(el, classList.slice(1).join(' '));
+};
+
+Util.removeClass = function(el, className) {
+	var classList = className.split(' ');
+	if (el.classList) el.classList.remove(classList[0]);	
+	else if(Util.hasClass(el, classList[0])) {
+		var reg = new RegExp('(\\s|^)' + classList[0] + '(\\s|$)');
+		el.className=el.className.replace(reg, ' ');
+	}
+	if (classList.length > 1) Util.removeClass(el, classList.slice(1).join(' '));
+};
+
+Util.toggleClass = function(el, className, bool) {
+	if(bool) Util.addClass(el, className);
+	else Util.removeClass(el, className);
+};
+
+Util.setAttributes = function(el, attrs) {
+  for(var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+};
+
+/* 
+  DOM manipulation
+*/
+Util.getChildrenByClassName = function(el, className) {
+  var children = el.children,
+    childrenByClass = [];
+  for (var i = 0; i < el.children.length; i++) {
+    if (Util.hasClass(el.children[i], className)) childrenByClass.push(el.children[i]);
+  }
+  return childrenByClass;
+};
+
+Util.is = function(elem, selector) {
+  if(selector.nodeType){
+    return elem === selector;
+  }
+
+  var qa = (typeof(selector) === 'string' ? document.querySelectorAll(selector) : selector),
+    length = qa.length,
+    returnArr = [];
+
+  while(length--){
+    if(qa[length] === elem){
+      return true;
+    }
+  }
+
+  return false;
+};
+
+/* 
+	Animate height of an element
+*/
+Util.setHeight = function(start, to, element, duration, cb) {
+	var change = to - start,
+	    currentTime = null;
+
+  var animateHeight = function(timestamp){  
+    if (!currentTime) currentTime = timestamp;         
+    var progress = timestamp - currentTime;
+    var val = parseInt((progress/duration)*change + start);
+    element.style.height = val+"px";
+    if(progress < duration) {
+        window.requestAnimationFrame(animateHeight);
+    } else {
+    	cb();
+    }
+  };
+  
+  //set the height of the element before starting animation -> fix bug on Safari
+  element.style.height = start+"px";
+  window.requestAnimationFrame(animateHeight);
+};
+
+/* 
+	Smooth Scroll
+*/
+
+Util.scrollTo = function(final, duration, cb) {
+  var start = window.scrollY || document.documentElement.scrollTop,
+      currentTime = null;
+      
+  var animateScroll = function(timestamp){
+  	if (!currentTime) currentTime = timestamp;        
+    var progress = timestamp - currentTime;
+    if(progress > duration) progress = duration;
+    var val = Math.easeInOutQuad(progress, start, final-start, duration);
+    window.scrollTo(0, val);
+    if(progress < duration) {
+        window.requestAnimationFrame(animateScroll);
+    } else {
+      cb && cb();
+    }
+  };
+
+  window.requestAnimationFrame(animateScroll);
+};
+
+/* 
+  Focus utility classes
+*/
+
+//Move focus to an element
+Util.moveFocus = function (element) {
+  if( !element ) element = document.getElementsByTagName("body")[0];
+  element.focus();
+  if (document.activeElement !== element) {
+    element.setAttribute('tabindex','-1');
+    element.focus();
+  }
+};
+
+/* 
+  Misc
+*/
+
+Util.getIndexInArray = function(array, el) {
+  return Array.prototype.indexOf.call(array, el);
+};
+
+Util.cssSupports = function(property, value) {
+  if('CSS' in window) {
+    return CSS.supports(property, value);
+  } else {
+    var jsProperty = property.replace(/-([a-z])/g, function (g) { return g[1].toUpperCase();});
+    return jsProperty in document.body.style;
+  }
+};
+
+// merge a set of user options into plugin defaults
+// https://gomakethings.com/vanilla-javascript-version-of-jquery-extend/
+Util.extend = function() {
+  // Variables
+  var extended = {};
+  var deep = false;
+  var i = 0;
+  var length = arguments.length;
+
+  // Check if a deep merge
+  if ( Object.prototype.toString.call( arguments[0] ) === '[object Boolean]' ) {
+    deep = arguments[0];
+    i++;
+  }
+
+  // Merge the object into the extended object
+  var merge = function (obj) {
+    for ( var prop in obj ) {
+      if ( Object.prototype.hasOwnProperty.call( obj, prop ) ) {
+        // If deep merge and property is an object, merge properties
+        if ( deep && Object.prototype.toString.call(obj[prop]) === '[object Object]' ) {
+          extended[prop] = extend( true, extended[prop], obj[prop] );
+        } else {
+          extended[prop] = obj[prop];
+        }
+      }
+    }
+  };
+
+  // Loop through each object and conduct a merge
+  for ( ; i < length; i++ ) {
+    var obj = arguments[i];
+    merge(obj);
+  }
+
+  return extended;
+};
+
+// Check if Reduced Motion is enabled
+Util.osHasReducedMotion = function() {
+  var matchMediaObj = window.matchMedia('(prefers-reduced-motion: reduce)');
+  if(matchMediaObj) return matchMediaObj.matches;
+  return false; // return false if not supported
+}; 
+
+/* 
+	Polyfills
+*/
+//Closest() method
+if (!Element.prototype.matches) {
+	Element.prototype.matches = Element.prototype.msMatchesSelector || Element.prototype.webkitMatchesSelector;
+}
+
+if (!Element.prototype.closest) {
+	Element.prototype.closest = function(s) {
+		var el = this;
+		if (!document.documentElement.contains(el)) return null;
+		do {
+			if (el.matches(s)) return el;
+			el = el.parentElement || el.parentNode;
+		} while (el !== null && el.nodeType === 1); 
+		return null;
+	};
+}
+
+//Custom Event() constructor
+if ( typeof window.CustomEvent !== "function" ) {
+
+  function CustomEvent ( event, params ) {
+    params = params || { bubbles: false, cancelable: false, detail: undefined };
+    var evt = document.createEvent( 'CustomEvent' );
+    evt.initCustomEvent( event, params.bubbles, params.cancelable, params.detail );
+    return evt;
+   }
+
+  CustomEvent.prototype = window.Event.prototype;
+
+  window.CustomEvent = CustomEvent;
+}
+
+/* 
+	Animation curves
+*/
+Math.easeInOutQuad = function (t, b, c, d) {
+	t /= d/2;
+	if (t < 1) return c/2*t*t + b;
+	t--;
+	return -c/2 * (t*(t-2) - 1) + b;
+};
+// File#: _1_btn-slide-fx
+(function() {
+  var BtnSlideFx = function(element) {
+		this.element = element;
+    this.hover = false; 
+		btnSlideFxEvents(this);
+	};
+
+  function btnSlideFxEvents(btn) {
+    btn.element.addEventListener('mouseenter', function(event){ // detect mouse hover
+      btn.hover = true;
+      triggerBtnSlideFxAnimation(btn.element, 'from');
+    });
+    btn.element.addEventListener('mouseleave', function(event){ // detect mouse leave
+      btn.hover = false;
+      triggerBtnSlideFxAnimation(btn.element, 'to');
+    });
+    btn.element.addEventListener('transitionend', function(event){ // reset btn classes at the end of enter/leave animation
+      resetBtnSlideFxAnimation(btn.element);
+    });
+  };
+
+  function getEnterDirection(element, event) { // return mouse movement direction
+    var deltaLeft = Math.abs(element.getBoundingClientRect().left - event.clientX),
+      deltaRight = Math.abs(element.getBoundingClientRect().right - event.clientX),
+      deltaTop = Math.abs(element.getBoundingClientRect().top - event.clientY),
+      deltaBottom = Math.abs(element.getBoundingClientRect().bottom - event.clientY);
+    var deltaXDir = (deltaLeft < deltaRight) ? 'left' : 'right',
+      deltaX = (deltaLeft < deltaRight) ? deltaLeft : deltaRight,
+      deltaYDir = (deltaTop < deltaBottom) ? 'top' : 'bottom',
+      deltaY = (deltaTop < deltaBottom) ? deltaTop : deltaBottom;
+    return (deltaX < deltaY) ? deltaXDir : deltaYDir;
+  };
+
+  function triggerBtnSlideFxAnimation(element, direction) { // trigger animation -> apply in/out and direction classes
+    var inStep = (direction == 'from') ? '-out' : '',
+      outStep = (direction == 'from') ? '' : '-out';
+    Util.removeClass(element, 'btn-slide-fx-hover'+inStep);
+    resetBtnSlideFxAnimation(element);
+    Util.addClass(element, 'btn--slide-fx-'+direction+'-'+getEnterDirection(element, event)); // set direction 
+    setTimeout(function(){Util.addClass(element, 'btn-slide-fx-animate');}, 5); // add transition
+    setTimeout(function(){Util.addClass(element, 'btn-slide-fx-hover'+outStep);}, 10); // trigger transition
+  };
+
+  function resetBtnSlideFxAnimation(element) { // remove animation classes
+    Util.removeClass(element, 'btn--slide-fx-from-left btn--slide-fx-from-right btn--slide-fx-from-bottom btn--slide-fx-from-top btn--slide-fx-to-left btn--slide-fx-to-right btn--slide-fx-to-bottom btn--slide-fx-to-top btn-slide-fx-animate');
+  };
+
+	//initialize the BtnSlideFx objects
+	var btnSlideFx = document.getElementsByClassName('js-btn--slide-fx');
+	if( btnSlideFx.length > 0 ) {
+		for( var i = 0; i < btnSlideFx.length; i++) {
+      (function(i){new BtnSlideFx(btnSlideFx[i]);})(i);
+		}
+  }
+}());
+// Tooltip
+
+// File#: _1_tooltip
+(function() {
+	var Tooltip = function(element) {
+		this.element = element;
+		this.tooltip = false;
+		this.tooltipIntervalId = false;
+		this.tooltipContent = this.element.getAttribute('title');
+		this.tooltipPosition = (this.element.getAttribute('data-tooltip-position')) ? this.element.getAttribute('data-tooltip-position') : 'top';
+		this.tooltipClasses = (this.element.getAttribute('data-tooltip-class')) ? this.element.getAttribute('data-tooltip-class') : false;
+		this.tooltipId = 'js-tooltip-element'; // id of the tooltip element -> trigger will have the same aria-describedby attr
+		// there are cases where you only need the aria-label -> SR do not need to read the tooltip content (e.g., footnotes)
+		this.tooltipDescription = (this.element.getAttribute('data-tooltip-describedby') && this.element.getAttribute('data-tooltip-describedby') == 'false') ? false : true; 
+
+		this.tooltipDelay = 300; // show tooltip after a delay (in ms)
+		this.tooltipDelta = 10; // distance beetwen tooltip and trigger element (in px)
+		this.tooltipTriggerHover = false;
+		// tooltp sticky option
+		this.tooltipSticky = (this.tooltipClasses && this.tooltipClasses.indexOf('tooltip--sticky') > -1);
+		this.tooltipHover = false;
+		if(this.tooltipSticky) {
+			this.tooltipHoverInterval = false;
+		}
+		initTooltip(this);
+	};
+
+	function initTooltip(tooltipObj) {
+		// reset trigger element
+		tooltipObj.element.removeAttribute('title');
+		tooltipObj.element.setAttribute('tabindex', '0');
+		// add event listeners
+		tooltipObj.element.addEventListener('mouseenter', handleEvent.bind(tooltipObj));
+		tooltipObj.element.addEventListener('focus', handleEvent.bind(tooltipObj));
+	};
+
+	function removeTooltipEvents(tooltipObj) {
+		// remove event listeners
+		tooltipObj.element.removeEventListener('mouseleave',  handleEvent.bind(tooltipObj));
+		tooltipObj.element.removeEventListener('blur',  handleEvent.bind(tooltipObj));
+	};
+
+	function handleEvent(event) {
+		// handle events
+		switch(event.type) {
+			case 'mouseenter':
+			case 'focus':
+				showTooltip(this, event);
+				break;
+			case 'mouseleave':
+			case 'blur':
+				checkTooltip(this);
+				break;
+		}
+	};
+
+	function showTooltip(tooltipObj, event) {
+		// tooltip has already been triggered
+		if(tooltipObj.tooltipIntervalId) return;
+		tooltipObj.tooltipTriggerHover = true;
+		// listen to close events
+		tooltipObj.element.addEventListener('mouseleave', handleEvent.bind(tooltipObj));
+		tooltipObj.element.addEventListener('blur', handleEvent.bind(tooltipObj));
+		// show tooltip with a delay
+		tooltipObj.tooltipIntervalId = setTimeout(function(){
+			createTooltip(tooltipObj);
+		}, tooltipObj.tooltipDelay);
+	};
+
+	function createTooltip(tooltipObj) {
+		tooltipObj.tooltip = document.getElementById(tooltipObj.tooltipId);
+		
+		if( !tooltipObj.tooltip ) { // tooltip element does not yet exist
+			tooltipObj.tooltip = document.createElement('div');
+			document.body.appendChild(tooltipObj.tooltip);
+		} 
+		
+		// reset tooltip content/position
+		Util.setAttributes(tooltipObj.tooltip, {'id': tooltipObj.tooltipId, 'class': 'tooltip tooltip--is-hidden js-tooltip', 'role': 'tooltip'});
+		tooltipObj.tooltip.innerHTML = tooltipObj.tooltipContent;
+		if(tooltipObj.tooltipDescription) tooltipObj.element.setAttribute('aria-describedby', tooltipObj.tooltipId);
+		if(tooltipObj.tooltipClasses) Util.addClass(tooltipObj.tooltip, tooltipObj.tooltipClasses);
+		if(tooltipObj.tooltipSticky) Util.addClass(tooltipObj.tooltip, 'tooltip--sticky');
+		placeTooltip(tooltipObj);
+		Util.removeClass(tooltipObj.tooltip, 'tooltip--is-hidden');
+
+		// if tooltip is sticky, listen to mouse events
+		if(!tooltipObj.tooltipSticky) return;
+		tooltipObj.tooltip.addEventListener('mouseenter', function cb(){
+			tooltipObj.tooltipHover = true;
+			if(tooltipObj.tooltipHoverInterval) {
+				clearInterval(tooltipObj.tooltipHoverInterval);
+				tooltipObj.tooltipHoverInterval = false;
+			}
+			tooltipObj.tooltip.removeEventListener('mouseenter', cb);
+			tooltipLeaveEvent(tooltipObj);
+		});
+	};
+
+	function tooltipLeaveEvent(tooltipObj) {
+		tooltipObj.tooltip.addEventListener('mouseleave', function cb(){
+			tooltipObj.tooltipHover = false;
+			tooltipObj.tooltip.removeEventListener('mouseleave', cb);
+			hideTooltip(tooltipObj);
+		});
+	};
+
+	function placeTooltip(tooltipObj) {
+		// set top and left position of the tooltip according to the data-tooltip-position attr of the trigger
+		var dimention = [tooltipObj.tooltip.offsetHeight, tooltipObj.tooltip.offsetWidth],
+			positionTrigger = tooltipObj.element.getBoundingClientRect(),
+			position = [],
+			scrollY = window.scrollY || window.pageYOffset;
+		
+		position['top'] = [ (positionTrigger.top - dimention[0] - tooltipObj.tooltipDelta + scrollY), (positionTrigger.right/2 + positionTrigger.left/2 - dimention[1]/2)];
+		position['bottom'] = [ (positionTrigger.bottom + tooltipObj.tooltipDelta + scrollY), (positionTrigger.right/2 + positionTrigger.left/2 - dimention[1]/2)];
+		position['left'] = [(positionTrigger.top/2 + positionTrigger.bottom/2 - dimention[0]/2 + scrollY), positionTrigger.left - dimention[1] - tooltipObj.tooltipDelta];
+		position['right'] = [(positionTrigger.top/2 + positionTrigger.bottom/2 - dimention[0]/2 + scrollY), positionTrigger.right + tooltipObj.tooltipDelta];
+		
+		var direction = tooltipObj.tooltipPosition;
+		if( direction == 'top' && position['top'][0] < scrollY) direction = 'bottom';
+		else if( direction == 'bottom' && position['bottom'][0] + tooltipObj.tooltipDelta + dimention[0] > scrollY + window.innerHeight) direction = 'top';
+		else if( direction == 'left' && position['left'][1] < 0 )  direction = 'right';
+		else if( direction == 'right' && position['right'][1] + dimention[1] > window.innerWidth ) direction = 'left';
+		
+		if(direction == 'top' || direction == 'bottom') {
+			if(position[direction][1] < 0 ) position[direction][1] = 0;
+			if(position[direction][1] + dimention[1] > window.innerWidth ) position[direction][1] = window.innerWidth - dimention[1];
+		}
+		tooltipObj.tooltip.style.top = position[direction][0]+'px';
+		tooltipObj.tooltip.style.left = position[direction][1]+'px';
+		Util.addClass(tooltipObj.tooltip, 'tooltip--'+direction);
+	};
+
+	function checkTooltip(tooltipObj) {
+		tooltipObj.tooltipTriggerHover = false;
+		if(!tooltipObj.tooltipSticky) hideTooltip(tooltipObj);
+		else {
+			if(tooltipObj.tooltipHover) return;
+			if(tooltipObj.tooltipHoverInterval) return;
+			tooltipObj.tooltipHoverInterval = setTimeout(function(){
+				hideTooltip(tooltipObj); 
+				tooltipObj.tooltipHoverInterval = false;
+			}, 300);
+		}
+	};
+
+	function hideTooltip(tooltipObj) {
+		if(tooltipObj.tooltipHover || tooltipObj.tooltipTriggerHover) return;
+		clearInterval(tooltipObj.tooltipIntervalId);
+		if(tooltipObj.tooltipHoverInterval) {
+			clearInterval(tooltipObj.tooltipHoverInterval);
+			tooltipObj.tooltipHoverInterval = false;
+		}
+		tooltipObj.tooltipIntervalId = false;
+		if(!tooltipObj.tooltip) return;
+		// hide tooltip
+		removeTooltip(tooltipObj);
+		// remove events
+		removeTooltipEvents(tooltipObj);
+	};
+
+	function removeTooltip(tooltipObj) {
+		Util.addClass(tooltipObj.tooltip, 'tooltip--is-hidden');
+		if(tooltipObj.tooltipDescription) tooltipObj.element.removeAttribute('aria-describedby');
+	};
+
+	window.Tooltip = Tooltip;
+
+	//initialize the Tooltip objects
+	var tooltips = document.getElementsByClassName('js-tooltip-trigger');
+	if( tooltips.length > 0 ) {
+		for( var i = 0; i < tooltips.length; i++) {
+			(function(i){new Tooltip(tooltips[i]);})(i);
+		}
+	}
+}());
